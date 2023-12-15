b6cd8cf43a78e99fd995e200569c20df23ab7996
masayukeeeee <masayukeeeee@gmail.com>
2023-12-15 21:15:20 +0900

    update

--- lua/msakai/core/keymaps.lua
+++ lua/msakai/core/keymaps.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode with jk" })
