87e070ddd03be95682fb4f0b527d7720f7fe612a
masayukeeeee <masayukeeeee@gmail.com>
2023-12-17 23:59:04 +0900

    update setting

--- lua/msakai/core/keymaps.lua
+++ lua/msakai/core/keymaps.lua
@@ -5,3 +5,13 @@ local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode with jk" })
+
+-- tab new file
+keymap.set("n", "<leader>tn", ":tabnew<CR>", { desc = "New tab" })
+-- tab close
+keymap.set("n", "<leader>te", ":tabclose<CR>", { desc = "Close tab" })
+-- tab next
+keymap.set("n", "<leader>to", ":tabnext<CR>", { desc = "Next tab" })
+-- tab previous
+keymap.set("n", "<leader>te", ":tabprevious<CR>", { desc = "Previous tab" })
+
